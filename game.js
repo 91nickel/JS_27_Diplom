f5a5e336bdd8fd4a35f200921cbefead0f57e7ce
91nickel <43931371+91nickel@users.noreply.github.com>
2019-04-01 16:21:21 +0300

    1

--- game.js
+++ game.js
@@ -213,3 +213,15 @@ class Level {
         }
     }
 }
+class Player {
+    constructor(){
+
+    }
+}
+
+const grid = [
+    new Array(3),
+    ['wall', 'wall', 'lava']
+    ];
+    const level = new Level(grid);
+    runLevel(level, DOMDisplay);
